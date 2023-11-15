2267e3d6aa8095b72671f5f5f80cfc7ca03a8d16
Gonzalo Azaldegi <me@azaldev.com>
2023-11-15 09:52:44 +0100

    Initial commit

---
+++ src/utils/config.js
@@ -0,0 +1,14 @@
+// config.js
+export default {
+    computer:{
+      screen: 1024
+    },
+    web: {
+      name: "Orbit of Imagination",
+    },
+    gh: {
+      account: "AzalDevX",
+      repos_per_page: 4,
+    },
+  };
+  
