346bd6de190ae6f9344f97bde8e53ac51088307f
Gonzalo Azaldegi <me@azaldev.com>
2023-11-30 20:02:09 +0100

    Added tailwind colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,24 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			'big-stone': {
+				'50': '#f5f7fa',
+				'100': '#eaeef4',
+				'200': '#d0dbe7',
+				'300': '#a6bcd3',
+				'400': '#7799b9',
+				'500': '#557ca2',
+				'600': '#426287',
+				'700': '#37506d',
+				'800': '#30455c',
+				'900': '#2c3c4e',
+				'950': '#1f2937',
+			},			
+			fontFamily: {
+				// sans: ['-apple-system', 'BlinkMacSystemFont', 'sans-serif'],
+			  },			
+		},
 	},
 	plugins: [],
 }
